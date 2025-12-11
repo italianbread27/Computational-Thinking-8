987685fb6f6ba6d9fcc191b40812e0954507c62a
italianbread27 <conradgandt@seattleacademy.org>
2025-12-11 19:54:10 +0000

    Message

---
+++ Projects/adventure.py
@@ -0,0 +1,9 @@
+answer1 = input("I done watching TV - where should we go? (outside or fortnite with da boiz )")
+if answer1 == "fortnite with da boiz" :
+
+    answer2 = input("Do you wanna drop, tilted or pleasant")
+    if answer2 == "Tilted":
+        print("We drop tilted")
+    if answer2 == "pleasant":
+        print ("we drop pleasant")
+
